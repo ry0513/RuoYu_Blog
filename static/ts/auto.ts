f5ba8691e1985b0b5812cfb8c07025affd200b41
ruoyu <rrcj123456@gmail.com>
2022-02-22 16:12:59 +0800

    v0.0.2 修改打包

--- static/ts/auto.ts
+++ static/ts/auto.ts
@@ -2,7 +2,7 @@ import sass from "node-sass";
 import uglifyJS from "uglify-js";
 import fs from "fs-extra";
 import { resolve } from "path";
-
+const watch = process.env.NODE_WATCH === "dev";
 /**
  *    css相关
  */
@@ -61,31 +61,6 @@ const autoCompile = (path = "../scss") => {
 };
 autoCompile();
 
-// 监听改变
-fs.watch(
-    resolve(__dirname, "../scss"),
-    {
-        recursive: true,
-    },
-    (eventType, filename) => {
-        if (!filename.includes(".scss")) return;
-        if (eventType === "change") {
-            setTimeout(() => {
-                compile(resolve(__dirname, "../scss", filename));
-            }, 100);
-        } else if (eventType === "rename") {
-            if (!fs.existsSync(resolve(__dirname, "../scss", filename))) {
-                fs.unlinkSync(resolve(__dirname, "../css", filename.replace(".scss", ".min.css")));
-                fs.unlinkSync(resolve(__dirname, "../css", filename.replace(".scss", ".css")));
-            } else {
-                setTimeout(() => {
-                    compile(resolve(__dirname, "../scss", filename));
-                }, 100);
-            }
-        }
-    }
-);
-
 /**
  *    js相关
  */
@@ -131,33 +106,63 @@ const autoUglify = (path = "../js") => {
 };
 autoUglify();
 
-// 监听js文件改变
-fs.watch(
-    resolve(__dirname, "../js"),
-    {
-        recursive: true,
-    },
-    (eventType, filename) => {
-        if (eventType === "change") {
-            uglify(resolve(__dirname, "../js", filename));
-        }
-    }
-);
-
-// 监听ts文件改变
-fs.watch(
-    resolve(__dirname, "../ts"),
-    {
-        recursive: true,
-    },
-    (eventType, filename) => {
-        if (!filename.includes(".ts")) return;
-        if (eventType === "rename") {
-            if (!fs.existsSync(resolve(__dirname, "../ts", filename))) {
-                fs.unlinkSync(resolve(__dirname, "../js", filename.replace(".ts", ".js")));
-                fs.unlinkSync(resolve(__dirname, "../js", filename.replace(".ts", ".min.js")));
-                fs.unlinkSync(resolve(__dirname, "../js", filename.replace(".ts", ".min.map")));
+/**
+ *    监听
+ */
+if (watch) {
+    // 监听scss文件改变
+    fs.watch(
+        resolve(__dirname, "../scss"),
+        {
+            recursive: true,
+        },
+        (eventType, filename) => {
+            if (!filename.includes(".scss")) return;
+            if (eventType === "change") {
+                setTimeout(() => {
+                    compile(resolve(__dirname, "../scss", filename));
+                }, 100);
+            } else if (eventType === "rename") {
+                if (!fs.existsSync(resolve(__dirname, "../scss", filename))) {
+                    fs.unlinkSync(resolve(__dirname, "../css", filename.replace(".scss", ".min.css")));
+                    fs.unlinkSync(resolve(__dirname, "../css", filename.replace(".scss", ".css")));
+                } else {
+                    setTimeout(() => {
+                        compile(resolve(__dirname, "../scss", filename));
+                    }, 100);
+                }
             }
         }
-    }
-);
+    );
+
+    // 监听js文件改变
+    fs.watch(
+        resolve(__dirname, "../js"),
+        {
+            recursive: true,
+        },
+        (eventType, filename) => {
+            if (eventType === "change") {
+                uglify(resolve(__dirname, "../js", filename));
+            }
+        }
+    );
+
+    // 监听ts文件改变
+    fs.watch(
+        resolve(__dirname, "../ts"),
+        {
+            recursive: true,
+        },
+        (eventType, filename) => {
+            if (!filename.includes(".ts")) return;
+            if (eventType === "rename") {
+                if (!fs.existsSync(resolve(__dirname, "../ts", filename))) {
+                    fs.unlinkSync(resolve(__dirname, "../js", filename.replace(".ts", ".js")));
+                    fs.unlinkSync(resolve(__dirname, "../js", filename.replace(".ts", ".min.js")));
+                    fs.unlinkSync(resolve(__dirname, "../js", filename.replace(".ts", ".min.map")));
+                }
+            }
+        }
+    );
+}
